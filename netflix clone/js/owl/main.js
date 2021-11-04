cd3520f8eb8c0f8cd20f631500bff1eeb5a9fd21
Abner1abc <abnercunha2247@gmail.com>
2021-11-04 11:30:44 -0300

    commit incial clone-netflix

---
+++ netflix clone/js/owl/main.js
@@ -0,0 +1,18 @@
+alert('Bem vindo');
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
